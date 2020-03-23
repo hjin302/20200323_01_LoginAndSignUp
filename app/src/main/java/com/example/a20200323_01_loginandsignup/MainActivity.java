6126a486b66356f1c7c4a21c6f6530cbe76ac936
hjin302 <enro302@naver.com>
2020-03-23 10:50:32 +0900

    아이디 체크 여부도 ContextUtil 활용해서 반영구 저장

--- app/src/main/java/com/example/a20200323_01_loginandsignup/MainActivity.java
+++ app/src/main/java/com/example/a20200323_01_loginandsignup/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.a20200323_01_loginandsignup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 
 import com.example.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
 import com.example.a20200323_01_loginandsignup.utils.ContextUtil;
@@ -15,20 +18,52 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         setupEvents();
         setValues();
+
     }
 
     @Override
     public void setupEvents() {
-//        로그인 버튼을 누르면 => 입력 되어있는 이메일 저장
+
+//        체크박스에 체크가 될때 (변화가 있을때) 마다
+//        체크 여부를 저장.
+
+        binding.idCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+//                ContextUtil을 이용해서, 체크 여부를 저장.
+                ContextUtil.setIdCheck(mContext, isChecked);
+
+            }
+        });
+
+
+//        로그인 버튼을 누르면 => 아이디 저장이 체크되어 있다면
+//        => 입력되어있는 이메일 저장
+//        그렇지 않다면 => 이메일을 빈칸 "" 으로 저장
         binding.loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String inputEmail = binding.emailEdt.getText().toString();
+            public void onClick(View v) {
 
-                ContextUtil.setEmail(mContext, inputEmail);
+//                체크박스에 체크가 되어있나?
+                if (binding.idCheckBox.isChecked()) {
+
+//                    체크가 되어 있는 상황
+                    String inputEmail = binding.emailEdt.getText().toString();
+
+                    ContextUtil.setEmail(mContext, inputEmail);
+                }
+                else {
+//                    체크가 안된 상황
+                    ContextUtil.setEmail(mContext, "");
+                }
+
+
+
             }
         });
 
@@ -36,7 +71,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-//      이 화면을 키면, 저장된 이메일 값을 emailEdt에 입력.
+
+//        이 화면을 키면, 저장된 이메일 값을 emailEdt에 입력.
         binding.emailEdt.setText(ContextUtil.getEmail(mContext));
+
+        binding.idCheckBox.setChecked(ContextUtil.isIdCheck(mContext));
     }
 }
--- app/src/main/java/com/example/a20200323_01_loginandsignup/utils/ContextUtil.java
+++ app/src/main/java/com/example/a20200323_01_loginandsignup/utils/ContextUtil.java
@@ -4,32 +4,51 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class ContextUtil {
-//    메모장파일 처럼 데이터를 저장할 공간의 이름으로 쓸 변수.
+
+    //    메모장파일 처럼 데이터를 저장할 공간의 이름으로 쓸 변수.
     private static final String prefName = "myPref";
 
-//    항목명도 자동완성 지원할 수 있도록 미리 변수화.
+    //    항목명도 자동완성 지원할수 있도록 미리 변수화.
     private static final String EMAIL = "EMAIL";
+    private static final String ID_CHECK = "ID_CHECK";
 
-//    해당 항목의 값을 저장 / 조회 하는 메소드 두개.
+//    해당 항목의 값을 저장(setter) / 조회(getter) 하는 메쏘드 두개.
 
-//    setter
-    public static void setEmail(Context context, String email){
-//        메모장을 이용해서 값을 기록 => 메모장 파일부터 열어야함.
+    //    setter
+    public static void setEmail(Context context, String email) {
+
+//        메모장을 이용해서 값을 기록. => 메모장 파일부터 열어야 함.
 //        메모장은 Context를 이용해서 열어야함. => Context변수도 파라미터로 필요함.
 
-//        메모장을 열때 1) 어떤메모장? PrefName 변수에 담아둠 2) 어떤 모드? - Context.MODE.PRIVATE
+//        메모장을 열때 1) 어떤메모장? prefName변수에 담아둠 2) 어떤 모드? - Context.MODE_PRIVATE
         SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
 
 //        열린 메모장에 항목(key)/값(value) 저장
         pref.edit().putString(EMAIL, email).apply();
+
     }
 
-//    getter
-    public static String getEmail(Context context){
+    //    getter
+    public static String getEmail(Context context) {
+//        메모장을 열어야 뭐라고 적혀있는지 확인 가능.
         SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
 
-//        EMAIL 항목에 적혀있는 값을 확인해서 바로 리턴처리
-//        저장되 앖이 없다면, 빈칸으로 주도록.
+//       EMAIL항목에 적혀있는 값을 확인해서 바로 리턴처리
+//        저장된 값이 없다면, 빈칸으로 주도록.
         return pref.getString(EMAIL, "");
     }
+
+
+    public static void setIdCheck(Context context, boolean isCheck) {
+        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
+        pref.edit().putBoolean(ID_CHECK, isCheck).apply();
+    }
+
+    public static boolean isIdCheck(Context context) {
+        SharedPreferences pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
+        return pref.getBoolean(ID_CHECK, true);
+    }
+
 }
+
+
